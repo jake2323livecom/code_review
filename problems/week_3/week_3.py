f0c3a8745a49b9fe8932e72ec96d2ce38412ce39
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2022-12-01 12:24:21 -0500

    Add first 2 challenges. Test push to Origin

---
+++ problems/week_3/week_3.py
@@ -0,0 +1,18 @@
+### Find An Easter Egg ###
+
+import this
+
+### Find The 3 Highest Numbers ###
+
+
+def numbers(list):
+    
+    highest_3 = list
+    highest_3.sort()
+    highest_3.reverse()
+    print(highest_3[0:3])
+    
+numbers([100, 4, 888, 432, 999])
+
+### Add a Number To Its Reverse ###
+
