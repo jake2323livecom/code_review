2b95fada63cc7a75241afcbd174d71eb8162f14f
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2022-12-05 15:08:38 -0600

    Update week_3.py

--- problems/week_3/week_3.py
+++ problems/week_3/week_3.py
@@ -1,45 +1,45 @@
-#     ##########################
-#     ### Find An Easter Egg ###
-#     ##########################
+     ##########################
+     ### Find An Easter Egg ###
+     ##########################
 
 
-# import this
+ import this
 
 
-#     ##################################
-#     ### Find The 3 Highest Numbers ###
-#     ##################################
+     ##################################
+     ### Find The 3 Highest Numbers ###
+     ##################################
 
 
-# def numbers(list):
+ def numbers(list):
+   
+     highest_3 = list
+     highest_3.sort()
+     highest_3.reverse()
+     print(highest_3[0:3])
     
-#     highest_3 = list
-#     highest_3.sort()
-#     highest_3.reverse()
-#     print(highest_3[0:3])
+ numbers([100, 4, 888, 432, 999])
+
+
+     ###################################
+     ### Add a Number To Its Reverse ###
+     ###################################
+
+
+ def reverse_number():
     
-# numbers([100, 4, 888, 432, 999])
-
-
-#     ###################################
-#     ### Add a Number To Its Reverse ###
-#     ###################################
-
-
-# def reverse_number():
-    
-#     num = int(input("Please Enter A Number: "))
-#     new_num = int((str(num)[::-1]))
-#     final_num = num + new_num
-#     return final_num
+     num = int(input("Please Enter A Number: "))
+     new_num = int((str(num)[::-1]))
+     final_num = num + new_num
+     return final_num
     
     
-# print(reverse_number())
+ print(reverse_number())
 
 
-#     #############################
-#     ### Old Fashioned Texting ###
-#     #############################
+     #############################
+     ### Old Fashioned Texting ###
+     #############################
    
    
 def old_fashion_text (string) :
