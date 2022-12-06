2e4c61249ebfa5f38234a95cc3b40bc99739e739
NCVito <119527972+NCVito@users.noreply.github.com>
2022-12-06 12:15:11 -0500

    Update CodeReview3.py

--- problems/week_3/CodeReview3.py
+++ problems/week_3/CodeReview3.py
@@ -16,6 +16,7 @@ highest_nums(nums)
 ## Add a number to its reverse #
 
 def reversi(num):
+    '''Adds a given number to itself with digits reversed'''
     num2 = int(str(num)[::-1])
     reversi_sum = num + num2
     return reversi_sum
