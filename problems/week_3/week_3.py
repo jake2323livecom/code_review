7ea6fe48d1200cb974daf0000660ac3c33f8db33
Jeremy King <Jeremy.King029@Gmail.com>
2022-12-06 11:14:53 -0500

    Update week_3.py with formatting for readibility

--- problems/week_3/week_3.py
+++ problems/week_3/week_3.py
@@ -1,49 +1,61 @@
-     ##########################
-     ### Find An Easter Egg ###
-     ##########################
+    ##########################
+    ### Find An Easter Egg ###
+    ##########################
 
 
- import this
+print('#' * 30 + " PART I " + '#' * 30)
+
+import this
+print()
 
 
-     ##################################
-     ### Find The 3 Highest Numbers ###
-     ##################################
+    ##################################
+    ### Find The 3 Highest Numbers ###
+    ##################################
 
 
- def numbers(list):
-   
-     highest_3 = list
-     highest_3.sort()
-     highest_3.reverse()
-     print(highest_3[0:3])
-    
- numbers([100, 4, 888, 432, 999])
+print('#' * 30 + " PART II " + '#' * 30)
+
+def numbers(list):
+
+    highest_3 = list
+    highest_3.sort()
+    highest_3.reverse()
+    print(highest_3[0:3])
+    print()
+
+numbers([100, 4, 888, 432, 999])
 
 
-     ###################################
-     ### Add a Number To Its Reverse ###
-     ###################################
+    ###################################
+    ### Add a Number To Its Reverse ###
+    ###################################
 
 
- def reverse_number():
-    
-     num = int(input("Please Enter A Number: "))
-     new_num = int((str(num)[::-1]))
-     final_num = num + new_num
-     return final_num
-    
-    
- print(reverse_number())
+print('#' * 30 + " PART III " + '#' * 30)
+
+def reverse_number():
+
+    num = int(input("Please Enter A Number: "))
+    print(f"Your number is: {num}")
+    new_num = int((str(num)[::-1]))
+    print(f"Your number in reverse is: {new_num}")
+    final_num = num + new_num
+    print(f"Your number plus your number in reverse is: {final_num}")
+    print()
+
+reverse_number()
 
 
-     #############################
-     ### Old Fashioned Texting ###
-     #############################
-   
-   
+    #############################
+    ### Old Fashioned Texting ###
+    #############################
+
+
+print('#' * 30 + " PART IV " + '#' * 30)
+
 def old_fashion_text (string) :
-    
+
     keypad = {
     '1' : ['.', ',', '?', '!', ':'],
     '2' : ['A', 'B', 'C'],
@@ -55,7 +67,7 @@ def old_fashion_text (string) :
     '8' : ['T', 'U', 'V'],
     '9' : ['W', 'X', 'Y', 'Z'],
     '0' : [' ']
-}
+    }
 
     txt = string.upper()
 
@@ -70,17 +82,19 @@ def old_fashion_text (string) :
                     result += '-'
 
     print(result)
+    print()
 
 old_fashion_text("she mad woke frfr")
 
 
-     #######################
-     ### Friday the 13th ###
-     #######################
-     
-     
-def friday_the_13th(range()):
-     pass
+    #######################
+    ### Friday the 13th ###
+    #######################
+    
+    
+print('#' * 30 + " PART IV " + '#' * 30)
 
-friday_the_13th(2010, 2022)
-     
+# def friday_the_13th(range()):
+#     pass
+
+# friday_the_13th(2010, 2022)
