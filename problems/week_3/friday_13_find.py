3b0c2fc6c2ad14da3a7b0e09afbf52be7e320a5a
John Fuller <john.adrift@gmail.com>
2022-12-06 12:13:47 -0500

    updates list comprehension

--- problems/week_3/friday_13_find.py
+++ problems/week_3/friday_13_find.py
@@ -12,18 +12,15 @@ def main():
 def fri_find(year:str) -> list[str]:
     ''' Returns the months that have a Friday the 13th for a given year'''
 
-    # Create integer-based list of months with friday the 13th
-    months = []
-    for month in range(1,13):
-        if date(int(year), month, 13).weekday() == 4:
-            months.append(month)
+    ##### Ridiculous list comprehension solution #####
+    return [calendar.month_name[month] for month in range(1,13) if date(int(year), month, 13).weekday() == 4]
 
-    ##### Optional solution for above code with a list comprehension #####
-    # months = [month for month in range(1,13) if date(int(year), month, 13).weekday() == 4]
-
-    # Return a human readable list of months with friday the 13th
-    return [calendar.month_name[month_num] for month_num in months]
-
+    ##### More easily readable solution #####
+    # months = []
+    # for month in range(1,13):
+    #     if date(int(year), month, 13).weekday() == 4:
+    #         months.append(month)
+    # return [calendar.month_name[month_num] for month_num in months]
 
 if __name__ == "__main__":
     main()
