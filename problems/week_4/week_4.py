af5e104f102a1533bc08463a863b4663bc330124
Jeremy King <Jeremy.King029@Gmail.com>
2022-12-12 13:28:21 -0500

    Update week_4.py

--- problems/week_4/week_4.py
+++ problems/week_4/week_4.py
@@ -1,31 +1,74 @@
 import requests
 from requests.auth import HTTPBasicAuth
+import urllib3
 
-def get_auth_token():
+urllib3.disable_warnings()
 
-    ''' Returns the authentication token for the Cisco DNAC Sandbox '''
 
-    url = "https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token"
+##################################################################################
+##################################################################################
+##################################################################################
 
-    response = requests.post(
+
+# def get_auth_token():
+
+#     ''' Returns the authentication token for the Cisco DNAC Sandbox '''
+
+#     url = "https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token"
+
+#     response = requests.post(
+#         url,
+#         auth=HTTPBasicAuth(
+#             username="devnetuser",
+#             password="Cisco123!"
+#             ), 
+#         verify=False,
+#     )
+
+#     token = response.json()
+  
+#     return token
+
+# if __name__ == "__main__":
+#     print(get_auth_token())
+
+
+##################################################################################
+##################################################################################
+##################################################################################
+
+
+def get_device_list():
+
+    ''' Returns the current device list for the Nautobot demo website '''
+
+    url = "https://demo.nautobot.com/api/dcim/devices/"
+    token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    headers={
+            "Authorization": "Bearer Token {}".format(token),
+            "Accept": "application/json; indent=4"
+    }
+
+    response = requests.get(
         url,
         auth=HTTPBasicAuth(
-            username="devnetuser",
-            password="Cisco123!"
-            ), 
-        verify=False,
-        # headers = {
-        #     "Content-Type": "application/json",
-        #     "Authorization": None,
-        #     "Accept": "application/json"
-        # }
+            username="demo",
+            password="nautobot",
+            ),
+        headers=headers,
+        verify=False
     )
 
-    token = response.json()
+    device_list = response.json()
 
-    # print(token)
-   
-    return token
+    return device_list
 
 if __name__ == "__main__":
-    print(get_auth_token())
+    print(get_device_list())
+
+print(__name__)
+
+
+# curl -H "Authorization: Token aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+# -H "Accept: application/json; indent=4" \
+# https://demo.nautobot.com/api/dcim/devices/
